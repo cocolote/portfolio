e54234f3266d6c92966d6d98e89a74d423da7efa
cocolote <skiel.j.lopez@gmail.com>
2015-08-16 09:52:25 -0400

    commit old changes

--- app.rb
+++ app.rb
@@ -2,11 +2,14 @@ require 'sinatra'
 require 'sinatra/activerecord'
 #this is dedicated to Spencer, great EE who show us the power of pry!
 require 'pry'
+require 'sass'
 
 ###############
 # CONTROLLERS #
 #  & ROUTES   #
 ###############
+
+get('/portfolio.css'){ scss :portfolio }
 
 get '/' do
   redirect '/home'
